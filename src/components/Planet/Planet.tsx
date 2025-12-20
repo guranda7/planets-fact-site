f92def8880b5cc418d44d4911e754032d23da2f0
guranda <gurandaggg@gmail.com>
2025-12-20 16:05:02 +0400

    import photos

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+
+import { useParams } from 'react-router-dom'
+import data from "../../data.json"
 
 export default function Planet() {
+  const {planetName} = useParams()
+  const planet = data.find((planet) => planet.name === planetName)
+  console.log(planetName)
   return (
-    <div>Planet</div>
+    <div>
+      <h2>{planet?.name}</h2>
+      <p>{planet?.overview.content}</p>
+    </div>
   )
 }
--- src/components/pages/Layout.tsx
+++ src/components/pages/Layout.tsx
@@ -1,7 +1,14 @@
 import Header from "../Header/Header"
 import { Outlet } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+
 
 export default function Layout() {
+  const navigate = useNavigate()
+  useEffect(()=> {
+    navigate("/Earth")
+  },[])
   return (
     <div>
         <Header/>
